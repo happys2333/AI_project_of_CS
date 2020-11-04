d9a0e6beacca935a4db4c764cba1ec77020767d5
开心 <56923707+happys2333@users.noreply.github.com>
2020-11-04 21:13:10 +0800

    Update test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-s = 'print(\'s=\',s,\'\\\'\');s = list(\'s=\\\'\'+s+\'\\\'\'+s);s = s.reverse()\n\
-for x in s:\n\
-    print(x,end=\'\')'
-print('s=\'',s,'\'',s)
-s = list('s=\''+s+'\''+s)
-s.reverse()
-for x in s:
-    print(x,end='')
